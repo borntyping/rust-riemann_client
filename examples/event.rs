e7fb705afe672d475fa82998a35894b2da62e83b
Uggla <uggla@free.fr>
2021-02-01 00:33:45 +0100

    feat: Cleanup and upgrade
    - Remove all warnings
    - Remove all clippy lint errors
    - Set edition to 2018
    - Remove extern crate invocation as not needed anymore with 2018 edition
    - Use protobuf 2.20.0 and regenerate mod_pb.rs using this version
    - Update libc to 0.2.84
    - Use docopt 1.1.0
    - Use serde 1.0.123 instead of rustc-serialize as not needed anymore by
      docopt
    - Set version to 0.7.0
    
    Tests are ok.
    Riemann-cli is working fine.
    Compiled https://github.com/hubblo-org/scaphandre with this updated
    version is working fine as well.

--- examples/event.rs
+++ examples/event.rs
@@ -1,18 +1,18 @@
-extern crate riemann_client;
-
-use riemann_client::Client;
 use riemann_client::proto::Event;
+use riemann_client::Client;
 
 fn main() {
     let mut client = Client::connect(&("localhost", 5555)).unwrap();
 
-    client.event({
-        let mut event = Event::new();
-        event.set_service("rust-riemann_client".to_string());
-        event.set_state("ok".to_string());
-        event.set_metric_d(128.128);
-        event
-    }).unwrap();
+    client
+        .event({
+            let mut event = Event::new();
+            event.set_service("rust-riemann_client".to_string());
+            event.set_state("ok".to_string());
+            event.set_metric_d(128.128);
+            event
+        })
+        .unwrap();
 
     // client.event(riemann_client::Event {
     //     service: "rust-riemann_client",
